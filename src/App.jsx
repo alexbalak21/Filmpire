1811790e14d54911764ac0077d234165d9617c9b
alexbalak21 <alex.balak@outlook.com>
2025-01-20 12:11:26 +0100

    styles implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
 import React from "react"
 import { CssBaseline } from "@mui/material"
 import { Routes, Route } from "react-router"
+
+import useStyles from "./styles"
+
 import { Actors, Movies, NavBar, Profile, MovieInformation } from "./components"
 
-const App = () => (
-  <main>
-    <CssBaseline />
-    <NavBar />
-    <Routes>
-      <Route path="/" element={<h1>Home</h1>} />
-      <Route path="/movies/:id" element={<MovieInformation />} />
-      <Route path="/actors/:id" element={<Actors />} />
-      <Route path="/profile/:id" element={<Profile />} />
-    </Routes>
-  </main>
-)
+const App = () => {
+  const classes = useStyles()
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <NavBar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/movies/:id" element={<MovieInformation />} />
+          <Route path="/actors/:id" element={<Actors />} />
+          <Route path="/profile/:id" element={<Profile />} />
+        </Routes>
+      </main>
+    </div>
+  )
+}
 
 export default App
