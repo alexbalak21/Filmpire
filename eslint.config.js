01c0425b7ee58a2cad46d9d171e14354dbbdb8ca
alexbalak21 <alex.balak@outlook.com>
2025-01-20 11:36:42 +0100

    working router

--- eslint.config.js
+++ eslint.config.js
@@ -1,37 +1,57 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import js from "@eslint/js"
+import globals from "globals"
+import react from "eslint-plugin-react"
+import reactHooks from "eslint-plugin-react-hooks"
+import reactRefresh from "eslint-plugin-react-refresh"
 
 export default [
-  { ignores: ['dist'] },
+  { ignores: ["dist"] },
   {
-    files: ['**/*.{js,jsx}'],
+    files: ["**/*.{js,jsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
       parserOptions: {
-        ecmaVersion: 'latest',
+        ecmaVersion: "latest",
         ecmaFeatures: { jsx: true },
-        sourceType: 'module',
+        sourceType: "module",
       },
     },
-    settings: { react: { version: '18.3' } },
+    settings: { react: { version: "18.3" } },
     plugins: {
       react,
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
     },
     rules: {
-      ...js.configs.recommended.rules,
-      ...react.configs.recommended.rules,
-      ...react.configs['jsx-runtime'].rules,
-      ...reactHooks.configs.recommended.rules,
-      'react/jsx-no-target-blank': 'off',
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
+      "import/extensions": 0,
+      "react/prop-types": 0,
+      "linebreak-style": 0,
+      "react/state-in-constructor": 0,
+      "import/prefer-default-export": 0,
+      "max-len": [2, 250],
+      "no-multiple-empty-lines": ["error", { max: 1, maxEOF: 1 }],
+      "no-underscore-dangle": [
+        "error",
+        { allow: ["_d", "_dh", "_h", "_id", "_m", "_n", "_t", "_text"] },
+      ],
+      "object-curly-newline": 0,
+      "react/jsx-filename-extension": 0,
+      "react/jsx-one-expression-per-line": 0,
+      "jsx-a11y/click-events-have-key-events": 0,
+      "jsx-a11y/alt-text": 0,
+      "jsx-a11y/no-autofocus": 0,
+      "jsx-a11y/no-static-element-interactions": 0,
+      "react/no-array-index-key": 0,
+      "no-param-reassign": 0,
+      "react/react-in-jsx-scope": 0,
+      "jsx-a11y/anchor-is-valid": [
+        "error",
+        {
+          components: ["Link"],
+          specialLink: ["to", "hrefLeft", "hrefRight"],
+          aspects: ["noHref", "invalidHref", "preferButton"],
+        },
       ],
     },
   },
