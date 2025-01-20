386c39420156a10929c58730a89ea7d89d539703
alexbalak21 <alex.balak@outlook.com>
2025-01-20 11:53:49 +0100

    Structure Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import React from "react"
 import { CssBaseline } from "@mui/material"
 import { Routes, Route } from "react-router"
+import { Actors, Movies, NavBar, Profile, MovieInformation } from "./components"
 
 const App = () => (
-  <div>
+  <main>
     <CssBaseline />
+    <NavBar />
     <Routes>
       <Route path="/" element={<h1>Home</h1>} />
-      <Route path="/about" element={<h1>About</h1>} />
+      <Route path="/movies/:id" element={<MovieInformation />} />
+      <Route path="/actors/:id" element={<Actors />} />
+      <Route path="/profile/:id" element={<Profile />} />
     </Routes>
-  </div>
+  </main>
 )
 
 export default App
