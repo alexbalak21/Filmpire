97667d3dc6fb699f2b64ed39a10c4deaa8062900
Alex Balak <alex.balak@outlook.com>
2025-01-30 23:03:31 +0100

    navbar done

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,46 @@
 import React from "react"
+import {AppBar, IconButton, Toolbar, Drawer, Button, Avatar, useMediaQuery} from "@mui/material"
+import {Menu, AccountCircle, Brightness4, Brightness7} from "@mui/icons-material"
+import {Link} from "react-router"
+import {useTheme} from "@mui/styles"
+
+import useStyles from "./styles"
 
 const NavBar = () => {
-  return <div>NavBar</div>
+  const classes = useStyles()
+  const isMobile = useMediaQuery("(max-width: 600px)")
+  const theme = useTheme()
+  const isAuthenticated = true
+  return (
+    <>
+      <AppBar position="fixed">
+        <Toolbar className={classes.toolbar}>
+          {isMobile && (
+            <IconButton edge="start" color="inherit" aria-label="menu" style={{outline: "none"}} onClick={() => {}} className={classes.menuButton}>
+              <Menu />
+            </IconButton>
+          )}
+          <IconButton color="inherit" sx={{ml: 1}} onClick={() => {}}>
+            {theme.palette.mode === "dark" ? <Brightness7 /> : <Brightness4 />}
+          </IconButton>
+          {!isMobile && "Search..."}
+          <div>
+            {!isAuthenticated ? (
+              <Button color="inherit" onClick={() => {}} to="/auth">
+                Login &nbsp; <AccountCircle />
+              </Button>
+            ) : (
+              <Button color="inherit" component={Link} to="/profile/:id" className={classes.linkButton}>
+                {!isMobile && <>My Movies &nbsp;</>}
+                <Avatar style={{width: 30, height: 30}} alt="Profile" src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" />
+              </Button>
+            )}
+          </div>
+          {isMobile && "Search..."}
+        </Toolbar>
+      </AppBar>
+    </>
+  )
 }
 
 export default NavBar
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,22 @@
-import { StrictMode } from "react"
-import { createRoot } from "react-dom/client"
-import { BrowserRouter } from "react-router"
+import {StrictMode} from "react"
+import {createRoot} from "react-dom/client"
+import {BrowserRouter} from "react-router"
+import {createTheme, ThemeProvider} from "@mui/material/styles"
+
 import App from "./App"
+
+const theme = createTheme({
+  palette: {
+    mode: "light",
+  },
+})
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </StrictMode>
 )
