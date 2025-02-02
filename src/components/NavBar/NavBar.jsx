376c86dca9f5f4058c71baa27da30669574bacc2
Alex Balak <alex.balak@outlook.com>
2025-02-02 23:22:51 +0100

    Progress on the drawer

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,25 +1,14 @@
 import React from "react"
-import {
-  AppBar,
-  IconButton,
-  Toolbar,
-  Drawer,
-  Button,
-  Avatar,
-  useMediaQuery,
-} from "@mui/material"
-import {
-  Menu,
-  AccountCircle,
-  Brightness4,
-  Brightness7,
-} from "@mui/icons-material"
-import { Link } from "react-router"
-import { useTheme } from "@mui/styles"
+import {useState} from "react"
+import {AppBar, IconButton, Toolbar, Drawer, Button, Avatar, useMediaQuery} from "@mui/material"
+import {Menu, AccountCircle, Brightness4, Brightness7} from "@mui/icons-material"
+import {Link} from "react-router"
+import {useTheme} from "@mui/styles"
 
 import useStyles from "./styles"
 
 const NavBar = () => {
+  const [mobileOpen, setMobileOpen] = useState(false)
   const classes = useStyles()
   const isMobile = useMediaQuery("(max-width: 600px)")
   const theme = useTheme()
@@ -29,18 +18,11 @@ const NavBar = () => {
       <AppBar position="fixed">
         <Toolbar className={classes.toolbar}>
           {isMobile && (
-            <IconButton
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              style={{ outline: "none" }}
-              onClick={() => {}}
-              className={classes.menuButton}
-            >
+            <IconButton edge="start" color="inherit" aria-label="menu" style={{outline: "none"}} onClick={() => {}} className={classes.menuButton}>
               <Menu />
             </IconButton>
           )}
-          <IconButton color="inherit" sx={{ ml: 1 }} onClick={() => {}}>
+          <IconButton color="inherit" sx={{ml: 1}} onClick={() => {}}>
             {theme.palette.mode === "dark" ? <Brightness7 /> : <Brightness4 />}
           </IconButton>
           {!isMobile && "Search..."}
@@ -50,18 +32,9 @@ const NavBar = () => {
                 Login &nbsp; <AccountCircle />
               </Button>
             ) : (
-              <Button
-                color="inherit"
-                component={Link}
-                to="/profile/:id"
-                className={classes.linkButton}
-              >
+              <Button color="inherit" component={Link} to="/profile/:id" className={classes.linkButton}>
                 {!isMobile && <>My Movies &nbsp;</>}
-                <Avatar
-                  style={{ width: 30, height: 30 }}
-                  alt="Profile"
-                  src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
-                />
+                <Avatar style={{width: 30, height: 30}} alt="Profile" src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" />
               </Button>
             )}
           </div>
@@ -70,7 +43,7 @@ const NavBar = () => {
       </AppBar>
       <div>
         <nav className={classes.drawer}></nav>
-        {isMobile ? <Drawer></Drawer> : <Drawer></Drawer>}
+        {isMobile ? <Drawer open={mobileOpen}></Drawer> : <Drawer></Drawer>}
       </div>
     </>
   )
